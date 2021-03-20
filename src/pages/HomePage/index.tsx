3dfe78d2ec15cfa0e67144bdd23ae722921d9890
Victor Calazans Ramos <victor.ramos@lactec.org.br>
2021-03-20 12:05:13 -0300

    foi

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,46 +1,79 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import FlipPage from "react-flip-page";
 
 import { Container } from './styles';
 
-import First from '../../assets/first.svg';
-import Second from '../../assets/second.svg';
-import Third from '../../assets/third.svg';
-import Four from '../../assets/four.svg';
+import First from '../../assets/firstmobile.png';
+import Second from '../../assets/secondmobile.png';
+import Third from '../../assets/thirdmobile.png';
+import Four from '../../assets/fourmobile.png';
 import Start from '../../assets/start.png';
+import FirstDesktop from '../../assets/first.png';
+import SecondDesktop from '../../assets/second.png';
 
 const HomePage: React.FC = () => {
   const [isHistory, setIsHistory] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   const handleCallingGame = () => {
     setIsHistory(true);
     console.log(isHistory);
   }
 
+  const windowWidth = (window.innerWidth);
+  const MIN_WINDOW_WIDTH = 980;
+
+  useEffect(() => {
+    if (windowWidth <= MIN_WINDOW_WIDTH) {
+      setIsMobile(true);
+    }
+    else {
+      setIsMobile(false);
+    }
+  }, [windowWidth]);
+
+
   return (
     <Container>
-      {isHistory ? (<></>) : 
-        (
-        <div className="first-container">
-          <div className="second-container">
-            <FlipPage noShadow={true} width="100%" height={window.innerHeight} orientation="horizontal">
-              <article className="firstArticle">
-                <img src={First} alt="first" className="firstImage" />
-              </article>
-              <article className="firstArticle">
-                <img src={Second} alt="first" className="firstImage" />
-              </article>
-              <article className="firstArticle">
-                <img src={Third} alt="first" className="firstImage" />
-              </article>
-              <article className="firstArticle">
-                <img src={Four} alt="first" className="firstImage" />
-                <img src={Start} alt="button" onClick={handleCallingGame} className="button" />
-              </article>
-            </FlipPage>
-          </div>
-        </div>)}
+      {isHistory ? (<>
+      </>) : <>
+        {!isMobile ? (
+          <div className="first-container">
+            <div className="second-container">
+              <FlipPage flipOnTouch={true} showSwipeHint={true} uncutPages width="100%" height={window.innerHeight} orientation="horizontal">
+                <article className="firstArticle">
+                  <img src={FirstDesktop} alt="first" className="firstImage" />
+                </article>
+                <article className="secondArticle">
+                  <img src={SecondDesktop} alt="first" className="firstImage" />
+                  <img src={Start} alt="button" onClick={handleCallingGame} className="buttonDesktop" />
+                </article>
+              </FlipPage>
+            </div>
+          </div>) :
+          (
+            <div className="first-container">
+              <div className="second-container">
+                <FlipPage flipOnTouch={true} disableSwipe={true} animationDuration={0} uncutPages noShadow={true} width="100%" height={window.innerHeight} orientation="horizontal">
+                  <article className="firstArticle">
+                    <img src={First} alt="first" className="firstImage" />
+                  </article>
+                  <article className="secondArticle">
+                    <img src={Second} alt="first" className="firstImage" />
+                  </article>
+                  <article className="thirdArticle">
+                    <img src={Third} alt="first" className="firstImage" />
+                  </article>
+                  <article className="fourArticle">
+                    <img src={Four} alt="first" className="fourImage" />
+                    <img src={Start} alt="button" onClick={handleCallingGame} className="button" />
+                  </article>
+                </FlipPage>
+              </div>
+            </div>)}
+      </>}
+
     </Container>
   )
 };
--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -7,12 +7,54 @@ export const Container = styled.div`
   .firstArticle {
     width: 100%;
     height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: #fcdeb9;
+  }
+
+  .secondArticle {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: #717c81;
+  }
+
+  .thirdArticle {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: #efaeb4;
+  }
+
+  .fourArticle {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    background: #584f71;
   }
 
   .firstImage {
     width: 100%;
-    height: 100%;
-    background: #FEEDD9;
+  }
+
+  .fourImage {
+    width: 100%;
   }
 
   > p {
@@ -20,34 +62,30 @@ export const Container = styled.div`
   }
 
   .button {
-    width: 130px;
-    height: 50px;
+      width: 130px;
+      height: 50px;
 
-    position: relative;
+      position: relative;
 
-    bottom: 132px;
-    left: calc(50vw - 65px);
-
-    /* z-index: 99; */
-
-    cursor: pointer;
-  }
+      bottom: 65px;
+      left: calc(50vw - 195px);
+      
+      cursor: pointer;
+    }
   
-  @media(max-width: 550px) {
+  /* @media(max-width: 550px) {
     .button {
       width: 130px;
       height: 50px;
 
       position: relative;
 
-      bottom: 132px;
-      left: calc(50vw - 65px);
-
-      /* z-index: 99; */
-
+      bottom: 65px;
+      left: calc(50vw - 195px);
+      
       cursor: pointer;
     }
-  }
+  } */
 
   @media(min-width: 980px) {
 
@@ -61,23 +99,85 @@ export const Container = styled.div`
     }
 
     .second-container {
-      width: 480px;
+      width: 100%;
     }
+    
 
-    .button {
+    .buttonDesktop {
       width: 130px;
       height: 50px;
 
       position: absolute;
 
-      top: 735px;
-      bottom: 0;
-      right: 0;
-      left: 20px;
-
-      z-index: 99;
+      /* bottom: calc(50vh - 222px);
+      right: calc(50vw - 340px); */
+      
+      bottom: 20%;
+      right: 20%;
 
       cursor: pointer;
     }
   }
+
+  @media(min-width: 1366px) {
+
+  .first-container {
+    width: 100%;
+    height: auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .second-container {
+    width: 1600px;
+  }
+
+
+  .buttonDesktop {
+    width: 130px;
+    height: 50px;
+
+    position: absolute;
+
+    /* bottom: calc(50vh - 250px);
+    right: calc(50vw - 435px); */
+    bottom: 20%;
+    right: 20%;
+
+    cursor: pointer;
+  }
+}
+
+@media(min-width: 1500px) {
+
+  .first-container {
+    width: 100%;
+    height: auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .second-container {
+    width: 1600px;
+  }
+
+
+  .buttonDesktop {
+    width: 130px;
+    height: 50px;
+
+    position: absolute;
+
+    /* bottom: calc(50vh - 280px);
+    right: calc(50vw - 475px); */
+    bottom: 20%;
+    right: 20%;
+
+    cursor: pointer;
+  }
+}
 `;
