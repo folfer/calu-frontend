816c984755439f7aff59378432641fa33f5b3ae2
folfer <victor.folfer@gmail.com>
2021-03-22 20:57:41 -0300

    [Code] add background

--- src/pages/GamePage/styles.js
+++ src/pages/GamePage/styles.js
@@ -1,5 +1,7 @@
-
 import styled from 'styled-components';
+import background_game from '../../assets/background.svg';
+import background_mobile from '../../assets/background.svg';
+
 
 export const Main = styled.div`
   width: 100%;
@@ -11,11 +13,48 @@ export const Main = styled.div`
 `;
 
 export const Container = styled.div`
-  width: 50%;
-  height: 100vh;
+  width: 100%;
+  height: 99vh;
 
   display: flex;
   align-items: center;
   justify-content: center;
+
+  background-image: url(${background_mobile});
+  background-repeat: no-repeat;
+  background-size: 200vh auto;
+  background-position: center top;
+  background-attachment: fixed;
+
+  @media(min-width: 766px) {
+    width: 100%;
+    height: 99vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-image: url(${background_mobile});
+    background-repeat: no-repeat;
+    background-size: 200vh auto;
+    background-position: center top;
+    background-attachment: fixed;
+  }
+
+
+  @media(min-width: 980px) {
+    width: 30%;
+    height: 99vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-image: url(${background_game});
+    background-repeat: no-repeat;
+    background-size: 100vw auto;
+    background-position: center top;
+    background-attachment: fixed;
+  }
 `;
 
